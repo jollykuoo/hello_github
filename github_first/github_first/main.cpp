a4bbda0a4eec1909ebfe098a3505c81d6baa9f48
Jolly <mac@Jolly.local>
2019-01-05 16:48:08 +0800

    github_first

---
+++ github_first/github_first/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  github_first
+//
+//  Created by Jolly on 2019/1/5.
+//  Copyright © 2019年 Jolly. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, Github !\n";
+    return 0;
+}
